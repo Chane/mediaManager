b1c9b6c4037d393cb9358f9b4301fb703b2260f4
Nikolai Dante <git@nathwilson.co.uk>
2021-09-04 15:28:39 +0100

    Test for ProvideAndCreateAsync
    Test for ProvideAndCreateAsync

--- Engine.Tests/VideoInfoProviderShould.cs
+++ Engine.Tests/VideoInfoProviderShould.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections.Generic;
 using System.IO.Abstractions;
 using System.Threading;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
+using Xabe.FFmpeg;
 
 namespace Engine.Tests
 {
@@ -14,14 +18,32 @@ namespace Engine.Tests
         // Read Metadata
         // store thumbs smaller
 
-        [Test, Ignore("WIP")]
+        [Test]
         public async Task ProvideAndCreate()
         {
             var tokenSource = new CancellationTokenSource();
             var fileSystem = new FileSystem();
-            var ffmpegWrapper = new FFmpegWrapper();
+
+            var mediaInfoMock = new Mock<IMediaInfo>();
+            mediaInfoMock.Setup(m => m.VideoStreams)
+                .Returns(new List<IVideoStream> { Mock.Of<IVideoStream>() });
+
+            var conversionResultMock = new Mock<IConversionResult>();
+            conversionResultMock.Setup(m => m.Duration)
+                .Returns(default(TimeSpan));
+
+            var ffmpegWrapper = new Mock<IFFmpegWrapper>();
+            ffmpegWrapper.Setup(m => m.ExtractNthFrame(
+                    It.IsAny<IVideoStream>(),
+                    It.IsAny<Func<string,string>>(),
+                    It.IsAny<int>(),
+                    tokenSource.Token))
+                .ReturnsAsync(conversionResultMock.Object);
+            ffmpegWrapper.Setup(m => m.GetMediaInfoAsync(It.IsAny<string>(), tokenSource.Token))
+                .ReturnsAsync(mediaInfoMock.Object);
+
             var filePath = string.Empty;
-            var provider = new VideoInfoProvider(fileSystem, ffmpegWrapper);
+            var provider = new VideoInfoProvider(fileSystem, ffmpegWrapper.Object);
             var videoDetail = await provider.ProvideAndCreateAsync(filePath, tokenSource.Token).ConfigureAwait(false);
 
             Assert.That(videoDetail.Created, Is.EqualTo(true));
