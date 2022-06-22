25009e5e86cf809070fd8406c1d090e0f6c5ac1d
Nikolai Dante <git@nathwilson.co.uk>
2022-06-22 14:47:45 +0100

    Add more tables

--- Engine.Storage/Schema/Migrations/Version1.cs
+++ Engine.Storage/Schema/Migrations/Version1.cs
@@ -8,7 +8,12 @@
             {
                 "CREATE TABLE VersionInformation ( Version INTEGER NOT NULL )",
                 "INSERT INTO VersionInformation (Version) VALUES (0)",
-                "CREATE TABLE DirectoryInfo ( Directory TEXT NOT NULL, File TEXT NOT NULL )",
+                "CREATE TABLE BaseDirectory ( BaseDirectoryId INT, Path TEXT NOT NULL )",
+                "CREATE TABLE FileType ( FileTypeId INT, FileTypeDescription TEXT )",
+                "INSERT INTO FileType ( FileTypeId , FileTypeDescription ) VALUES (1, 'Image'), (2, 'Video')",
+                "CREATE TABLE FileInfo ( " +
+                    "FileTypeId INT, BaseDirectoryId INT, Directory TEXT NOT NULL, File TEXT NOT NULL, " +
+                    "Thumbnail BLOB, FileWidth INT, FileHeight INT, FileLength INT, FileSize INT )",
             };
         }
     }
